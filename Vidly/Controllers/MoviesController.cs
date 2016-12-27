62ec4f9fb96052a0608b2e8c3ecdaee9881345d8
Iurii Faevskii <urfaevskii@gmail.com>
2016-12-27 15:53:14 +0200

    Reload progect and update the domain model

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        //
+        // GET: /Movies/
+
+        public ActionResult Index()
+        {
+            var movies = GetMovies();
+            return View(movies);    
+        }
+
+        public ActionResult Details(int id)
+        {
+            var movie = GetMovies().SingleOrDefault(m => m.Id == id);
+
+            if(movie == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(movie);
+        }
+
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Name = "Shrek!"
+            };
+            var customers = new List<Customer>
+            {
+                new Customer{Name="Customer 1"},
+                new Customer{Name="Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie{Id = 1, Name="Shrek"},
+                new Movie{Id = 2, Name="Wall-e"}
+            };
+        }
+
+
+	}
+}
