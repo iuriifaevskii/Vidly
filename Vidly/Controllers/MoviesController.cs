1fd01e1f59cc0272743dde549f02eee16b189bae
Iurii Faevskii <urfaevskii@gmail.com>
2017-01-06 20:49:17 +0200

    Restrict other movie manage operations to store manager.

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -34,6 +34,7 @@ namespace Vidly.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Save(Movie movie)
         {
             if(!ModelState.IsValid)
@@ -71,6 +72,7 @@ namespace Vidly.Controllers
             
             return View("ReadOnlyList");
         }
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Edit(int id)
         {
             var movie = _context.Movies.SingleOrDefault( m => m.Id == id);
