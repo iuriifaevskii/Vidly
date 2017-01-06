a9d7f0538e1f4f1a751bf704ce0e2b84009c66c7
Iurii Faevskii <urfaevskii@gmail.com>
2017-01-06 12:53:21 +0200

    Add membership type to customer API and fix bootbox.

--- Vidly/Dtos/CustomerDto.cs
+++ Vidly/Dtos/CustomerDto.cs
@@ -19,6 +19,8 @@ namespace Vidly.Dtos
 
         public byte MembershipTypeId { get; set; }
 
+        public MembershipTypeDto membershipType { get; set; }
+
         //[Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
     }
